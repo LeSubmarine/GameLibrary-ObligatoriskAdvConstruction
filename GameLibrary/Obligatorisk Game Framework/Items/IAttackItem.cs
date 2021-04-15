854d6bbcb560ccc441809cc9428eb3dd4028a8a2
LeSubmarine <henrik@osterby.dk>
2021-04-15 14:30:19 +0200

    Update IAttackItem.cs

--- GameLibrary/Obligatorisk Game Framework/Items/IAttackItem.cs
+++ GameLibrary/Obligatorisk Game Framework/Items/IAttackItem.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Obligatorisk_Game_Framework.Combat;
 
 namespace Obligatorisk_Game_Framework.Items
 {
-    public interface IAttackItem : IWearable
+    public interface IAttackItem : IWearable,IDamageDealing
     {
         #region Properties
         public double Power { get; set; }
