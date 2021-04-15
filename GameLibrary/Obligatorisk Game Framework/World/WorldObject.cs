bf002745b54d9d544a1576d2ec95d9513d5e91da
LeSubmarine <henrik@osterby.dk>
2021-04-15 10:48:24 +0200

    GetNeighbors done

--- GameLibrary/Obligatorisk Game Framework/World/WorldObject.cs
+++ GameLibrary/Obligatorisk Game Framework/World/WorldObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Obligatorisk_Game_Framework.Responses;
 
@@ -57,16 +58,64 @@ namespace Obligatorisk_Game_Framework.World
         #endregion
 
 
+        #region Overloaded Operators
+        /// <summary>
+        /// Checks if two WorldObjects have same id.
+        /// </summary>
+        /// <param name="a">First WorldObject.</param>
+        /// <param name="b">Second WorldObject</param>
+        /// <returns>Returns a true if the two objects have same equal Id property.</returns>
+        public static bool operator ==(WorldObject a, WorldObject b)
+        {
+            return a.Id == b.Id;
+        }
+
+        /// <summary>
+        /// Checks if two WorldObjects does not have same id.
+        /// </summary>
+        /// <param name="a">First WorldObject.</param>
+        /// <param name="b">Second WorldObject</param>
+        /// <returns>Returns a false if the two objects have same equal Id property, otherwise true.</returns>
+        public static bool operator !=(WorldObject a, WorldObject b)
+        {
+            return !(a == b);
+        }
+        #endregion
+
+
         #region Methods
         /// <summary>
-        /// 
+        /// Gets the neighbors of this object within a given the world it lives in, within a given range.
         /// </summary>
-        /// <param name="range"></param>
-        /// <param name="inhabitedWorld"></param>
-        /// <returns></returns>
-        public WorldObjectResponse GetNeighbours(double range, World inhabitedWorld)
+        /// <param name="range">The range to find neighbors.</param>
+        /// <param name="inhabitedWorld">The world the object lives in.</param>
+        /// <returns>Returns an IResponse with a IEnumerable containing the neighbors if any.</returns>
+        public WorldObjectResponse GetNeighbors(double range, World inhabitedWorld)
         {
-            var o
+            IEnumerable<WorldObject> otherObjects = inhabitedWorld.WorldObjectsManager.GetWorldObjects();
+            var neighbors =
+                from worldObject in otherObjects
+                where worldObject != this &&
+                      worldObject.Position.X < this.Position.X + range &&
+                      worldObject.Position.X > this.Position.X - range &&
+                      worldObject.Position.Y < this.Position.Y + range &&
+                      worldObject.Position.Y > this.Position.Y - range
+                select worldObject;
+            if (neighbors.Any())
+            {
+                return new WorldObjectResponse
+                {
+                    Description = $"These are the neighbors of the WorldObject: {Name}, Id: {Id}.",
+                    SuccessValue = true,
+                    WorldObjects = neighbors
+                }; 
+            }
+            return new WorldObjectResponse
+            {
+                Description = $"The WorldObject: {Name}, Id: {Id}, did not have any neighbors within the range of {range}, with the coordinates X:{Position.X},Y:{Position.Y}.",
+                SuccessValue = false,
+                WorldObjects = null
+            };
         }
         #endregion
     }
