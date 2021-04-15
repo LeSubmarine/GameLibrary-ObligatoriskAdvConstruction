58f7948eb8289ee04a572c2b504d78728ab4334e
LeSubmarine <henrik@osterby.dk>
2021-04-15 10:52:41 +0200

    Update EquipItemResponse.cs

--- GameLibrary/Obligatorisk Game Framework/Responses/EquipItemResponse.cs
+++ GameLibrary/Obligatorisk Game Framework/Responses/EquipItemResponse.cs
@@ -55,19 +55,5 @@ namespace Obligatorisk_Game_Framework.Responses
         /// </summary>
         public string Slot { get; set; }
         #endregion
-
-
-        #region Methods
-        /// <summary>
-        /// Creates a string based on the information available to describe the events of the operation.
-        /// </summary>
-        /// <returns>A string telling how the transaction went, and who the participants was.</returns>
-        public override string ToString()
-        {
-            //TODO make ToString to describe journey
-            StringBuilder returnString = new StringBuilder();
-            return "";
-        }
-        #endregion
     }
 }
