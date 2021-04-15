ff47d72784124341d4bb62e6612e802b9886f88d
LeSubmarine <henrik@osterby.dk>
2021-04-15 14:39:57 +0200

    Update IDefenseItem.cs

--- GameLibrary/Obligatorisk Game Framework/Items/IDefenseItem.cs
+++ GameLibrary/Obligatorisk Game Framework/Items/IDefenseItem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Obligatorisk_Game_Framework.Combat.DamageTypes;
+using Obligatorisk_Game_Framework.Responses.CombatResponses;
 
 namespace Obligatorisk_Game_Framework.Items
 {
@@ -8,6 +10,12 @@ namespace Obligatorisk_Game_Framework.Items
     {
         #region Properties
         public double Defense { get; set; }
+        public IEnumerable<IDamageType> Types { get; set; }
+        #endregion
+
+
+        #region Methods
+        public DamageResponse Defend(DamageResponse damage);
         #endregion
     }
 
